6ebf551c8de3070c09d18d02d8816526b1c4a37f
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2024-07-10 11:05:46 -0400

    make collate_fn a passable argument

--- auto_circuit/data.py
+++ auto_circuit/data.py
@@ -149,6 +149,7 @@ class PromptDataLoader(DataLoader[PromptPairBatch]):
         kv_cache: Optional[HookedTransformerKeyValueCache] = None,
         seq_labels: Optional[List[str]] = None,
         word_idxs: Dict[str, int] = {},
+        collate_fn = collate_fn,
         **kwargs: Any,
     ):
         """
